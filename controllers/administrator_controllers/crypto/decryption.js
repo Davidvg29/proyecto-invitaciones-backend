07951704b61144d31c599b5b549ed87c56053f77
David Valdez Gramajo <davidpivito@gmail.com>
2024-12-21 18:08:05 -0300

    npm crypto and controller crypto

--- controllers/administrator_controllers/crypto/decryption.js
+++ controllers/administrator_controllers/crypto/decryption.js
@@ -1,3 +1,13 @@
-module.exports = decryption = ()=>{
+const bcrypt = require("bcrypt")
 
+module.exports = decryption = async(someOtherPlaintextPassword, hash)=>{
+    try {
+        const compare = await bcrypt.compare(someOtherPlaintextPassword, hash)
+        return compare
+    } catch (error) {
+        return {
+            message: "Error al comparar contraseña",
+            error: error.message
+        }
+    }
 }
--- controllers/administrator_controllers/crypto/encryption.js
+++ controllers/administrator_controllers/crypto/encryption.js
@@ -1,3 +1,13 @@
-module.exports = encryption = ()=>{
+const bcrypt = requrie("bcrypt")
 
+module.exports = encryption = async(myPlaintextPassword)=>{
+    try {
+        const hash = await bcrypt.hash(myPlaintextPassword, 10)
+        return hash
+    } catch (error) {
+        return {
+            message:"Erro al encriptar la contraseña",
+            error: error.message
+        }
+    }
 }
