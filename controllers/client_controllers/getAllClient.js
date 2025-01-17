c61da612e60422fb10d35e7e46806cccabe454bc
David Valdez Gramajo <86501090+Davidvg29@users.noreply.github.com>
2025-01-17 19:39:52 -0300

    I change what it returns to me and get a password

--- controllers/client_controllers/getAllClient.js
+++ controllers/client_controllers/getAllClient.js
@@ -2,7 +2,16 @@ const {client} = require("../../db")
 
 const getAllClient = async(req, res)=>{
     try {
-        const clients = await client.findAll()
+        const clients = await client.findAll({
+            attributes: [
+                 "id_client",
+                 "user_client",
+                 "name_client",
+                 "phone_number_client",
+                 "createdAt",
+                 "updatedAt"
+            ]
+        })
         res.status(200).json({clients})
     } catch (error) {
         req.status(500).json({
