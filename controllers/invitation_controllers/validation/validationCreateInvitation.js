e35f9a7fd1f7ea6a42dfcd2c9ef0db6dfaec5f1e
David Valdez Gramajo <86501090+Davidvg29@users.noreply.github.com>
2025-02-03 17:52:09 -0300

    change location of name_invitation conditional

--- controllers/invitation_controllers/validation/validationCreateInvitation.js
+++ controllers/invitation_controllers/validation/validationCreateInvitation.js
@@ -3,8 +3,8 @@ module.exports = validationCreateInvitation = (id_client, id_plan, name_invitati
 
     if(!id_client || isNaN(Number(id_client)) || !Number.isInteger(Number(id_client))) error.id_client = "ID cliente tiene que ser un numero entero"
     if(!id_plan || isNaN(Number(id_plan)) || !Number.isInteger(Number(id_plan))) error.id_plan = "ID plan tiene que ser un numero entero"
-    if(!name_invitation || name_invitation.length < 3) error.name_invitation = "Nombre de invitacion debe contener al menos 3 caracteres"
     if(!name_invitation || name_invitation.length > 50) error.name_invitation = "Nombre de invitacion no puede contener mas de 50 caracteres"
+    if(!name_invitation || name_invitation.length < 3 || name_invitation.length === 0) error.name_invitation = "Nombre de invitacion debe contener al menos 3 caracteres"
     if(!codeHtml) error.codeHtml = "Codigo HTML no puede estar vacio"
 
     // Retorna false si no hay errores, o el objeto error si hay errores
